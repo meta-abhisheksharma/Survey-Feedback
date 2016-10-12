62f354e6b5833d7970f50f2fe11c8e848390e8fc
kuldeep <kuldeep.joshi@metacube.com>
2016-10-12 15:41:45 +0530

    changes directory structure

---
+++ src/main/java/com/survey/model/SurveyQuestions.java
@@ -0,0 +1,15 @@
+package main.java.com.survey.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Entity
+@Table(name="surveys_questions")
+public class SurveyQuestions {
+
+	private String surveyID;
+	private String questionID;
+	
+}
