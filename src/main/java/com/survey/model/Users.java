c8237521ee7213362f617ea3f9b825f522c90b02
meta-astha-gupta <meta-astha-gupta@users.noreply.github.com>
2016-10-13 13:37:05 +0530

    Update Users.java

--- src/main/java/com/survey/model/Users.java
+++ src/main/java/com/survey/model/Users.java
@@ -1,42 +1,73 @@
 package main.java.com.survey.model;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.JoinColumn;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import org.springframework.stereotype.Repository;
 
+import org.springframework.stereotype.Repository;
 
 @Repository
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class Users {
-	
+
 	@Id
-	@Column(name="userID",length=30)
+	@Column(name = "userID", length = 30, nullable = false)
 	private String userId;
-	@Column(name="name",nullable=false,length=45)
-	private	String name;
-	@Column(name="email",nullable=false,length=90)
+	@Column(name = "name", nullable = false, length = 45)
+	private String name;
+	@Column(name = "email", nullable = false, length = 90)
 	private String email;
-	
-	@Column(name="password",nullable=false,length=45)
+
+	@Column(name = "password", nullable = false, length = 45)
 	private String password;
-	
-	@Column(name="userRole",nullable=false,length=15)
+
+	@Column(name = "userRole", nullable = false, length = 15)
 	private String userRole;
-	
-	@Column(name="blockUser",length=1)
+
+	@Column(name = "blockUser", length = 1)
 	private String blockUser;
-	@Temporal(TemporalType.DATE)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date createdTime;
-	@Temporal(TemporalType.DATE)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date updatedTime;
-	
-	
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinTable(name = "users_surveys", joinColumns = @JoinColumn(name = "userID"), inverseJoinColumns = @JoinColumn(name = "surveyID"))
+	private Surveys survey = new Surveys();
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinTable(name = "users_responses", joinColumns = @JoinColumn(name = "userID"), inverseJoinColumns = @JoinColumn(name = "responseID"))
+	private Responses responses = new Responses();
+
+	@OneToMany(mappedBy = "createdBy")
+	private Set<Options> createOption = new HashSet<Options>();
+
+	@OneToMany(mappedBy = "updatedBy")
+	private Set<Options> updateOption = new HashSet<Options>();
+
+	@OneToMany(mappedBy = "createdBy")
+	private Set<Question> createQuestion = new HashSet<Question>();
+
+	@OneToMany(mappedBy = "updatedBy")
+	private Set<Question> updateQuestion = new HashSet<Question>();
+
+	@OneToMany(mappedBy = "createdBy")
+	private Set<UsersUsers> createdByUserList = new HashSet<UsersUsers>();
+
+	@OneToMany(mappedBy = "createdTo")
+	private Set<UsersUsers> createdToUserList = new HashSet<UsersUsers>();
 
 }
