fc8b25474560731943766ca4b97ccedbb11807ef
meta-nikhil-srivastava <nikhil.srivastava@metacube.com>
2016-10-18 00:27:28 +0530

    Dashboard UI

--- src/main/webapp/WEB-INF/Views/assets/js/main.js
+++ src/main/webapp/WEB-INF/Views/assets/js/main.js
@@ -1,7 +1,1 @@
 var myApp = angular.module("myApp",["ui.router","ngRoute"]);
-
-app.config(function($sceDelegateProvider) {
-   $sceDelegateProvider.resourceUrlWhitelist([
-       'http://localhost:8080/survey/**'
-   ]);
-});
