37a7c4ebacc416d9043dedc59ab693e7dbcf8e72
kuldeep <kuldeep.joshi@metacube.com>
2016-10-18 11:17:01 +0530

    changes

--- src/main/webapp/app/controller/loginController.js
+++ src/main/webapp/app/controller/loginController.js
@@ -2,7 +2,7 @@ myApp.controller('loginController',function($scope, $rootScope, $location,$http)
 	
 	$scope.saveUser=function(){
 		var obj={
-				userId:'1',name:'kuldeep',password:'pass'
+				userId:new Date(),name:'kuldeep',password:'pass'
 		};
 		$http.post('/rest/user/save',obj).success(function(res){
 			alert("ok");
@@ -10,4 +10,10 @@ myApp.controller('loginController',function($scope, $rootScope, $location,$http)
 			alert("not ok");
 		});
 	}
+	
+	$http.post('/rest/user/getAll').success(function(res){
+		alert("ok");
+	}).error(function(res){
+		alert("not ok");
+	});
 });
